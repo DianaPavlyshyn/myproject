6fb68606328411cf8eb103eaa6caeed2cf91db94
DianaPavlyshyn <dianapavlyshyn@gmail.com>
2019-08-12 03:22:51 +0000

    sample code

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//courses page
+document.querySelector('.timestamp').innerText = (new Date()).toLocaleTimeString()1;
+document.querySelector('ajax-html').addEventListener('click',getHtmlAjax);
+function getHtmlAjax () {
+    
+}
