5f45c5a5f01d060a20c439362823eae39b204f45
DianaPavlyshyn <dianapavlyshyn@gmail.com>
2019-08-16 03:45:46 +0000

    sample

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 //courses page
-document.querySelector('.timestamp').innerText = (new Date()).toLocaleTimeString()1;
-document.querySelector('ajax-html').addEventListener('click',getHtmlAjax);
-function getHtmlAjax () {
-    
+document.querySelector('.fetch-html').addEventListener('click', fetchHtml);
+
+function fetchHtml() {
+    fetch('client-data.html')
+        .then(response => response.text())
+        .then(html => document.querySelector('.html-container').innerHTML = html);
+}
+
+document.querySelector('.fetch-htmln').addEventListener('click', fetchHtmln);
+
+function fetchHtmln() {
+    fetch('offer.html')
+        .then(response => response.text())
+        .then(html => document.querySelector('.html-containern').innerHTML = html);
 }
